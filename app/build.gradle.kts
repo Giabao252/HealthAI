8273d5b988207c58d62a64e7fa9cbac92a6807f7
Giabao252 <luugiabaoluu24@gmail.com>
2025-10-02 00:33:37 -0400

    google services and authentication configured

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
 
     // Add the dependency for the Firebase Authentication library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth")
 
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
